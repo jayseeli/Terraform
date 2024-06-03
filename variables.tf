ee4687211fcbc0bd2d8f29b17fef6be2994af2ef
jayseeli <144837931+jayseeli@users.noreply.github.com>
2024-06-03 14:42:51 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project"{
+    type = string
+    description = "Project name."
+    default = "my-project"
+}
+
+variable "location"{
+    type = string
+    description = "Azure Region"
+    default = "eastus"
+}
